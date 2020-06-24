63b144dc3f875b22cc65f62aa43ed1610d2a6e9b
INDERJEET <inderbittu00@gmail.com>
2020-06-24 22:42:03 +0530

    Updates

---
+++ precache-manifest.f13285e2266e9af25b5424a922306c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cd7c3e3fe1c2dba3d78e45ca52a631",
+    "url": "/Chat-ON-Line/index.html"
+  },
+  {
+    "revision": "7c95f3e759af305fe02e",
+    "url": "/Chat-ON-Line/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb64f956905e0ed3a805",
+    "url": "/Chat-ON-Line/static/js/2.577633c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chat-ON-Line/static/js/2.577633c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c95f3e759af305fe02e",
+    "url": "/Chat-ON-Line/static/js/main.6001a2f5.chunk.js"
+  },
+  {
+    "revision": "5e6e4573646ee8e03818",
+    "url": "/Chat-ON-Line/static/js/runtime-main.a8a3ad59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chat-ON-Line/static/media/logo.5d5d9eef.svg"
+  }
+]);
